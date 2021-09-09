7137b0ed50112a3d0074d0f29c23184c29265f09
cintyagg <cintyagg100@gmail.com>
2021-09-09 20:38:20 -0300

    [FEATURE] - Create xml of login screen

---
+++ app/src/main/java/project/com/inflix_android/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package project.com.inflix_android.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.com.inflix_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
