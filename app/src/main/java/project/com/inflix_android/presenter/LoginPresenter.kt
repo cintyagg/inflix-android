846947fed7eb904b413061184a37203a9b76f35f
cintyagg <cintyagg100@gmail.com>
2021-09-22 23:28:45 -0300

    [FEATURE] - Update navigation

--- app/src/main/java/project/com/inflix_android/presenter/LoginPresenter.kt
+++ app/src/main/java/project/com/inflix_android/presenter/LoginPresenter.kt
@@ -1,5 +1,6 @@
 package project.com.inflix_android.presenter
 
+import project.com.inflix_android.R
 import project.com.inflix_android.model.User
 import project.com.inflix_android.view.LoginViewInterface
 
@@ -7,10 +8,10 @@ class LoginPresenter(private var LoginViewInterface: LoginViewInterface) : Login
     override fun onLogin(email: String, password: String) {
         val user = User(email,password)
         when (user.isDataValid()) {
-            0 -> LoginViewInterface.onLoginError("Email must not be null")
-            1 -> LoginViewInterface.onLoginError("Wrong email address")
-            2 -> LoginViewInterface.onLoginError("Password must have to contain 8 numbers")
-            else -> LoginViewInterface.onLoginSuccess("Login Success")
+            0 -> LoginViewInterface.onLoginError(R.string.email_not_null)
+            1 -> LoginViewInterface.onLoginError(R.string.wrong_email)
+            2 -> LoginViewInterface.onLoginError(R.string.wrong_password)
+            else -> LoginViewInterface.onLoginSuccess(R.string.login_success)
         }
     }
 }
--- app/src/main/java/project/com/inflix_android/ui/fragment/LoginFragment.kt
+++ app/src/main/java/project/com/inflix_android/ui/fragment/LoginFragment.kt
@@ -1,21 +1,21 @@
 package project.com.inflix_android.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.fragment_login.*
 import project.com.inflix_android.R
 import project.com.inflix_android.presenter.LoginPresenter
 import project.com.inflix_android.presenter.LoginPresenterInterface
+import project.com.inflix_android.ui.activity.MainActivity
 import project.com.inflix_android.view.LoginViewInterface
 
-class LoginFragment : Fragment(), LoginViewInterface {
+class LoginFragment : NavHostFragment(), LoginViewInterface {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +29,6 @@ class LoginFragment : Fragment(), LoginViewInterface {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
-    }
-
-    private val control by lazy{
-        findNavController()
     }
 
     lateinit var loginPresenter : LoginPresenterInterface
@@ -50,14 +46,18 @@ class LoginFragment : Fragment(), LoginViewInterface {
                 login_editTextNumberPassword.editText?.text.toString()
             )
         }
+        login_register_account.setOnClickListener {
+            val act = activity as MainActivity
+            act.showRegister()
+        }
     }
 
-    override fun onLoginSuccess(message: String) {
-        //control.navigate(R.id.homeFragment)
-        activity?.let { Toasty.success(it, message, Toast.LENGTH_SHORT).show() }
+    override fun onLoginSuccess(message: Int) {
+        val act = activity as MainActivity
+        act.showHome()
     }
 
-    override fun onLoginError(message: String) {
+    override fun onLoginError(message: Int) {
         activity?.let { Toasty.error(it, message, Toast.LENGTH_SHORT).show() }
     }
 }
